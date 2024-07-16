617919cacf9e452ea561d4221f56ae9413b9782e
sanatzzz <sanat250193@gmail.com>
2024-07-16 12:47:49 +0530

    Update AOSTest.java

--- src/test/java/test/example/AOSTest.java
+++ src/test/java/test/example/AOSTest.java
@@ -15,7 +15,7 @@ public class AOSTest {
         Thread.sleep(5000);
         WebElement profile =driver.findElement(By.id("menuUser"));
         profile.click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         WebElement username=driver.findElement(By.name("username"));
         WebElement password=driver.findElement(By.name("password"));
         username.sendKeys("admin");
