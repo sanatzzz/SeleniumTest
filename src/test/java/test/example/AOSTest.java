161097d874d2c3f00dea1de3bd752775437d3da2
sanatzzz <sanat250193@gmail.com>
2024-07-16 13:21:10 +0530

    Update AOSTest.java

--- src/test/java/test/example/AOSTest.java
+++ src/test/java/test/example/AOSTest.java
@@ -13,7 +13,7 @@ public class AOSTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.advantageonlineshopping.com/#/");
-        Thread.sleep(5000);
+        Thread.sleep(10000);
         WebElement profile =driver.findElement(By.id("menuUser"));
         profile.click();
         Thread.sleep(4000);
